4f17b2a1b15e94ab554a19430b5edbec0ae5f4aa
SarthakeChoudhary <sarthak.choudhary24@gmail.com>
2016-12-11 17:51:26 +0530

    added

---
+++ infix_to_post_pre.cpp
@@ -0,0 +1,168 @@
+#include<iostream>
+#include<cstdio>
+#include<string.h>
+using namespace std;
+class Stack
+{
+    int top;
+    char s[10];
+    public:
+    Stack()
+    {
+
+        top=-1;
+    }
+    void push(char c)
+    {
+        if(top==9)
+        {
+
+            cout<<"stack full";
+        }
+        else{
+            s[++top]=c;
+        }
+    }
+    char pop()
+    {
+
+        if(top==-1)
+        {
+            cout<<"stack empty";
+        }
+        else{
+            return s[top--];
+        }
+    }
+    void infix_to_post(char infix[])
+    {
+        int index=0;
+        char post[50]={'\0'};
+        char symbol;
+        char temp;
+        int pos=0;
+        int length=strlen(infix);
+        while(index<length)
+        {
+
+            symbol=infix[index];
+            switch(symbol)
+            {
+
+                case '(':push(symbol);
+                       break;
+                case ')':
+                    temp=pop();
+                    while(temp!='(')
+                    {
+
+                        post[pos]=temp;
+                        pos++;
+                        temp=pop();
+                    }
+                    break;
+                    case '+':
+                    case '-':
+                    case '/':
+                    case '*':
+                    case '%':
+                        while(preced(s[top])>=preced(symbol))
+                        {
+                            temp=pop();
+                            post[pos]=temp;
+                            pos++;
+                        }
+                        push(symbol);
+                        break;
+                        default:post[pos++]=symbol;
+            }
+            index++;
+        }
+        while(top>=0)
+        {
+            post[pos++]=pop();
+        }
+        puts(post);
+    }
+
+
+    void infix_to_pre(char infix[])
+    {
+        int index=0;
+        int pos=0;
+        char pre[50]={'\0'};
+        char symbol;
+        char temp;
+        int length=strlen(infix);
+        while(index<length)
+        {
+
+            symbol=infix[index];
+            switch(symbol)
+            {
+
+                case ')':push(symbol);
+                       break;
+                case '(':
+                    temp=pop();
+                    while(temp!=')')
+                    {
+
+                        pre[pos++]=temp;
+                        temp=pop();
+                    }
+                    break;
+                    case '+':
+                    case '-':
+                    case '/':
+                    case '*':
+                    case '%':
+                        while(preced(s[top])>=preced(symbol))
+                        {
+                            temp=pop();
+                            pre[pos++]=temp;
+                        }
+                        push(symbol);
+                        break;
+                        default:pre[pos++]=symbol;
+            }
+            index++;
+        }
+        while(top>=0)
+        {
+            pre[pos++]=pop();
+        }
+        strrev(pre);
+        puts(pre);
+    }
+    int preced(char ch)
+ {if(ch=='/'||ch=='*'||ch=='%')
+   return 5;
+  else
+   if(ch=='+'||ch=='-')
+    return 4;
+   else
+    return 3;
+ }
+
+
+};
+int main()
+{
+    char infix[50];
+    Stack obj;
+    cout<<"enter infix";
+    gets(infix);
+    cout<<"enter 1 for postfix ,2 for prefix";
+    int x;
+    cin>>x;
+
+    if(x==1)
+        obj.infix_to_post(infix);
+        else
+        {
+            strrev(infix);
+            obj.infix_to_pre(infix);
+        }
+}
+
